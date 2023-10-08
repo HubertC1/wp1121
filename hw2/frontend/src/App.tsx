87d69b781706c2fe5b3919f4736d9af79ca188fd
HubertC1 <bb.chang@gmail.com>
2023-10-08 16:13:52 +0800

    change to wp music

--- hw2/frontend/src/App.tsx
+++ hw2/frontend/src/App.tsx
@@ -11,6 +11,7 @@ import useCards from "@/hooks/useCards";
 function App() {
   const { lists, fetchLists, fetchCards } = useCards();
   const [newListDialogOpen, setNewListDialogOpen] = useState(false);
+  const [deletingList, setDeletingList] = useState(false);
 
   useEffect(() => {
     fetchLists();
@@ -21,19 +22,30 @@ function App() {
     <>
       <HeaderBar />
       <main className="mx-auto flex max-h-full flex-row gap-6 px-24 py-12">
-        {lists.map((list) => (
-          <CardList key={list.id} {...list} />
-        ))}
         <div>
+          <h1>My Playlist</h1>
           <Button
             variant="contained"
             className="w-80"
             onClick={() => setNewListDialogOpen(true)}
           >
             <AddIcon className="mr-2" />
-            Add a list
+            Add
+          </Button>
+          <br/>
+          <br/>
+          <Button
+            variant="contained"
+            className="w-80"
+            onClick={() => setDeletingList(true)}
+          >
+            <AddIcon className="mr-2" />
+            Delete
           </Button>
         </div>
+        {lists.map((list) => (
+          <CardList key={list.id} {...list} />
+        ))}
         <NewListDialog
           open={newListDialogOpen}
           onClose={() => setNewListDialogOpen(false)}
