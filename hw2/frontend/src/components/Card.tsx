420cf6f8ee5422d4865b83b19583831b0046d2ff
HubertC1 <bb.chang@gmail.com>
2023-10-10 12:07:01 +0800

    clickable url

--- hw2/frontend/src/components/Card.tsx
+++ hw2/frontend/src/components/Card.tsx
@@ -4,7 +4,9 @@ import { Paper } from "@mui/material";
 
 import CardDialog from "./CardDialog";
 
-import { List } from "@mui/icons-material";
+import { Link } from "@mui/icons-material";
+
+import { List, CheckBox } from "@mui/icons-material";
 
 export type CardProps = {
   id: string;
@@ -23,20 +25,34 @@ export default function Card({ id, title, description, singer, url, listId, onCa
     setOpen(true);
   };
 
+  const clickUrl = new URL(url);
+
   return (
     <main className="flex flex-row">
-      <input
-        type="checkbox"
-        value="checked"
-        onChange={() => onCardCheck(id)}
-      />
+      <Paper className="flex w-full flex-row p-2" elevation={6}>
+        <input
+          type="checkbox"
+          value="checked"
+          onChange={() => onCardCheck(id)}
+        />
+      {/* <CheckBox
+        // checked = {checked}
+        color="success"
+        // value = "checked"
+        onChange = {()=>onCardCheck(id)}
+      /> */}
       
-      <button onClick={handleClickOpen} className="text-start w-full" >
-        <Paper className="flex w-full flex-col p-2" elevation={6}>
-          {title}
-          {}
-        </Paper>
-      </button>
+        <button onClick={handleClickOpen} className="text-start w-full flex flex-row" >
+          
+            <main className="px-12">
+              {title}
+            </main>
+            <main className="mx-auto">
+              {singer}
+            </main>
+        </button>
+        <a href={url} target="_blank">{url}</a>
+      </Paper>
       <CardDialog
         variant="edit"
         open={open}
